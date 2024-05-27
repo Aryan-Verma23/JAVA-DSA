4389220883ba8cead731b9916e007e9198ce4089
Aryan Verma <aryan9.verma09@gmail.com>
2024-05-27 15:44:24 +0530

    Updated

--- 1-Conditionalloops/src/code for conditionsloops/fibbonacci.java
+++ 1-Conditionalloops/src/code for conditionsloops/fibbonacci.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-public class fibbonacci {
-    public static void main(String[] args) {
+public class fibbonacci 
+{
+    public static void main(String[] args) 
+    {
         try(Scanner sc = new Scanner(System.in) // Ensure the scanner is closed
         ) {
         //Q:find nth fibbonacci number?
@@ -10,26 +12,17 @@ public class fibbonacci {
 
         int a =0,b=1;
 
-        for (int i = 0; i < G; i++) {
+        for (int i = 0; i < G; i++)
+            {
             System.out.print(a + " ");
 
             int c = a + b;
             a = b ;
             b=c;
             
-        }
-
-        
-
-        
-        
-        
-
-
+           }
+   
+          }
+    
     }
-
-    
-    
-}
-
 }
