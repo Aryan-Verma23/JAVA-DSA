0ec6f7b75e56574c2a9ea029f12d498b199166f9
billlxclin <aryan9.verma09@gmail.com>
2024-05-31 22:21:52 +0530

    go 2

---
+++ 1-Conditionalloops/src/switch comms/SwitchNew.java
@@ -0,0 +1,24 @@
+public class SwitchNew {
+
+    public class SwitchExample {
+        public static void main(String[] args) {
+            int dayOfWeek = 3;
+            String dayName;
+    
+            dayName = switch (dayOfWeek) {
+                case 1 -> "Sunday";
+                case 2 -> "Monday";
+                case 3 -> "Tuesday";
+                case 4 -> "Wednesday";
+                case 5 -> "Thursday";
+                case 6 -> "Friday";
+                case 7 -> "Saturday";
+                default -> "Invalid day";
+            };
+    
+            System.out.println("Day of the week is: " + dayName);
+        }
+    }
+    
+
+}
