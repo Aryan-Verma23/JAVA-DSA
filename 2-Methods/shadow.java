a25a7f9cf5512447a52f21996483296424497904
billlxclin <aryan9.verma09@gmail.com>
2024-11-30 13:50:17 +0530

    var

---
+++ 2-Methods/shadow.java
@@ -0,0 +1,17 @@
+public class shadow {
+    static int x = 90;
+    public static void main(String[] args) {
+        System.out.println(x);
+        int x = 10;
+        System.out.println(x);
+        fun();
+                
+            
+            
+            }
+     static void fun() {
+            System.out.println(x);
+            }
+
+
+}
