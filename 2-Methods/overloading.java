7cc9fa6b580e301445b9e4f3b8ee841ddfeceb57
billlxclin <aryan9.verma09@gmail.com>
2024-11-30 13:59:02 +0530

    over

--- 2-Methods/overloading.java
+++ 2-Methods/overloading.java
@@ -1,12 +1,23 @@
 public class overloading {
     public static void main(String[] args) {
         fun("aryan");
+        fun(69);
+        System.out.println(sum(69,10));
+        System.out.println(sum(10,20,30));
     }
     static void fun(int a ){
         System.out.println(a);
     }
     static void fun(String name){
         System.out.println(name);
+    }
+    static int  sum(int a ,int b){
+        return(a+b);
+
+    }
+    static int  sum(int a ,int b,int c){
+        return(a+b+c);
+
     }
 
 }
